aee734ea00772d32e9105c677d88c3d84bad93fa
ravirasadiya <ravirasadiya1994@gmail.com>
2020-04-16 13:58:33 +0530

    Initial Commit

---
+++ 3_Longest_Substring_Without_Repeating_Characters.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        vector<int> dict(256, -1);
+        int maxLen = 0, start = -1;
+        for (int i = 0; i != s.length(); i++) {
+            if (dict[s[i]] > start)
+                start = dict[s[i]];
+            dict[s[i]] = i;
+            maxLen = max(maxLen, i - start);
+        }
+        return maxLen;
+    }
+};
+
+int main() {
+    Solution sol1;
+    string s ="abcde";
+    cout << sol1.lengthOfLongestSubstring(s);
+}
